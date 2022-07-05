aeba20bc1ccc407bd80dfb5d4cfcd3a4905f7357
jagadeesh8790 <jagadeesh.maddela93@gmail.com>
2022-07-05 22:47:34 +0530

    docker conatiner

---
+++ main.tf
@@ -0,0 +1,26 @@
+terraform{
+  required_provider{
+  docker = {
+    source = "kruezwerker/docker"
+    version = "2.17.0"	
+   }
+  }  
+ } 
+ provider "docker"{
+  host = "unix:///var.run.docker.sock"
+ }
+#pulls the image 
+resource "docker_image" "ubuntu" {
+  name = "ubuntu:latest"
+}
+resource "docker_container" "webserver" {
+  image = "${docker_image.ubuntu.latest}"
+  name = "terraform-docker-test"
+  must_run = true
+  publish_all_ports = true
+  command = [
+    "tail",
+    "-f",
+    "/dev/null"
+  ]
+}

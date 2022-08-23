6de64efa21219d577f92201ad3ec614f0d7e5120
jagadeesh8790 <jagadeesh.maddela93@gmail.com>
2022-08-23 15:49:19 +0530

    file added

--- subscribe.tf
+++ subscribe.tf
@@ -11,7 +11,7 @@ provider "aws" {
   region = "ap-south-1"
 }
 
-resorce "aws_budgets_budget" "like-and subscribe" {
+resource "aws_budgets_budget" "like-and subscribe" {
   name              = "monthly-budget"
   budget_type       = "COST"
   limit_amount      = "100.0"

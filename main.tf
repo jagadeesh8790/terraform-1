b654d09ea96132ed425fb2a911ef5312c08b367c
jagadeesh8790 <jagadeesh.maddela93@gmail.com>
2022-08-23 16:29:16 +0530

    mai file edited

--- main.tf
+++ main.tf
@@ -13,11 +13,11 @@ provider "aws" {
   secret_key = "a06ja9Gb+QeoXszXQbY1g0sJIs4NIXjTMubvdoOz"
 }
 
-resource "aws_instance" "my-UBUNTU"{
-	ami = "ami-02b401fea53d5bc05"
-	insatnce_type = "t2.micro"
+resource "aws_instance" "my-ubuntu1" {
+  ami           = "ami-02b401fea53d5bc05"
+  instance_type = "t2.micro"
 }
 
-resource "aws_eip" "myip"{
-	instance = "${aws_instance.my-UBUNTU.id}"
+resource "aws_eip" "myip" {
+  instance = aws_instance.my-ubuntu1.id
 }

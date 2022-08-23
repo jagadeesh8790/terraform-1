24e243055e9394071cccaccf0bc1db6240d8e52d
jagadeesh8790 <jagadeesh.maddela93@gmail.com>
2022-08-23 11:26:07 +0530

    updated to aws provider

--- main.tf
+++ main.tf
@@ -1,17 +1,17 @@
 terraform {
   required_providers {
-    docker = {
-      source  = "kreuzwerker/docker"
-      version = "~> 2.13.0"
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
     }
   }
 }
+
 
 provider "aws" {
   region = "ap-south-1"
 }
 
-# Specify the EC2 details
 resource "aws_instance" "example" {
   ami           = "ami-0b99c7725b9484f9e"
   instance_type = "t2.micro"
